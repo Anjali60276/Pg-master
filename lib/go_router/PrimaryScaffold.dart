412a0215fb8c40e15c15d375027c551017ed4c5c
sujay.png <147098677+sujay-png@users.noreply.github.com>
2026-07-08 11:41:04 +0530

    Updated GoRouter shell layout and scaffold

--- lib/go_router/PrimaryScaffold.dart
+++ lib/go_router/PrimaryScaffold.dart
@@ -1,22 +1,122 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class PrimaryScaffold extends StatelessWidget {
-
   final Widget body;
+  final String currentLocation;
 
   const PrimaryScaffold({
     super.key,
     required this.body,
+    required this.currentLocation,
   });
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Colors.black,
-      body: body,
+      body: Row(
+        children: [
+          // Sidebar
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Material(
+              color: const Color.fromARGB(125, 39, 32, 32),
+              child: Container(
+                width: 250,
+                child: Column(
+                  children: [
+                    const SizedBox(height: 20),
+                    Row(
+                      children: [
+                        const SizedBox(width: 15),
+                        const CircleAvatar(
+                          backgroundImage: AssetImage("assets/images/logo.jpg"),
+                        ),
+                        const SizedBox(width: 15),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              "PGMASTER",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              "Manager",
+                              style: TextStyle(
+                                color: Colors.grey.shade50,
+                                fontSize: 10,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 40),
+                    const Divider(color: Colors.deepOrange),
+                    ListTile(
+                      leading: const Icon(
+                        Icons.dashboard_outlined,
+                        color: Colors.black,
+                      ),
+                      title: const Text(
+                        "Dashboard",
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 185, 182, 182),
+                        ),
+                      ),
+                      selected: currentLocation == '/dashboard',
+                      selectedTileColor: Colors.deepOrange,
+                      onTap: () {
+                        context.go('/dashboard');
+                      },
+                    ),
+                    ListTile(
+                      leading: const Icon(
+                        Icons.people_outline,
+                        color: Colors.black,
+                      ),
+                      title: const Text(
+                        "Tenants",
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 185, 182, 182),
+                        ),
+                      ),
+                      selected: currentLocation == '/tenants',
+                      selectedTileColor: Colors.deepOrangeAccent,
+                      onTap: () {
+                        context.go('/tenants');
+                      },
+                    ),
+                    ListTile(
+                      leading: const Icon(
+                        Icons.payment_outlined,
+                        color: Colors.black,
+                      ),
+                      title: const Text(
+                        "Fees Collection",
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 185, 182, 182),
+                        ),
+                      ),
+                      selected: currentLocation == '/fees',
+                      selectedTileColor: Colors.deepOrange,
+                      onTap: () {
+                        context.go('/fees');
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(child: body),
+        ],
+      ),
     );
-
   }
-
 }
--- lib/go_router/shell_page.dart
+++ lib/go_router/shell_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:pgmaster/go_router/PrimaryScaffold.dart';
+
 
 class ShellPage extends StatelessWidget {
   final Widget child;
@@ -7,132 +9,13 @@ class ShellPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //to get the current route (URL path) from GoRouter.
-    //If the current page is /dashboard → location becomes "/dashboard"
-    // If the current page is /tenants → location becomes "/tenants"
-    // If the current page is /fees → location becomes "/fees"
-    // Used to identify which page is currently active
-    // Get the current route path (e.g., /dashboard, /tenants, /fees)
+    // Get current route location
     final String location = GoRouterState.of(context).uri.path;
-    return Scaffold(
-      body: Row(
-        children: [
-          // Sidebar
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Material(
-              color: const Color.fromARGB(125, 39, 32, 32),
-              child: Container(
-                width: 250,
-                child: Column(
-                  children: [
-                    const SizedBox(height: 20),
-
-                    Row(
-                      children: [
-                        const SizedBox(width: 15),
-                        const CircleAvatar(
-                          backgroundImage: AssetImage("assets/images/logo.jpg"),
-                        ),
-                        const SizedBox(width: 15),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Text(
-                              "PGMASTER",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              "Manager",
-                              style: TextStyle(
-                                color: Colors.grey.shade50,
-                                fontSize: 10,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-
-                    const SizedBox(height: 40),
-
-                    const Divider(color: Colors.deepOrange),
-
-                    ListTile(
-                      leading: const Icon(
-                        Icons.dashboard_outlined,
-                        color: Colors.black,
-                      ),
-                      title: const Text(
-                        "Dashboard",
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 185, 182, 182),
-                        ),
-                      ),
-                      // switching one index means to dashboard
-                      selected: location == '/dashboard',
-                      selectedTileColor: Colors.deepOrange,
-                      onTap: () {
-                        context.go('/dashboard'); //its call the dasboard pages
-                      },
-                    ),
-
-                    ListTile(
-                      leading: const Icon(
-                        Icons.people_outline,
-                        color: Colors.black,
-                      ),
-                      title: const Text(
-                        "Tenants",
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 185, 182, 182),
-                        ),
-                      ),
-
-                      selected:
-                          location == '/tenants', //its called the tenants page
-                      selectedTileColor: Colors.deepOrangeAccent,
-                      onTap: () {
-                        context.go(
-                          '/tenants',
-                        ); //here its route connected and calling function of router
-                      },
-                    ),
-
-                    ListTile(
-                      leading: const Icon(
-                        Icons.payment_outlined,
-                        color: Colors.black,
-                      ),
-                      title: const Text(
-                        "Fees Collection",
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 185, 182, 182),
-                        ),
-                      ),
-
-                      //it can highlight item if the current routes is fes or not
-                      selected: location == '/fees',
-                      selectedTileColor: Colors.deepOrange,
-                      onTap: () {
-                        context.go('/fees');
-                      },
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-
-          Expanded(child: child),
-
-          // Content Area
-        ],
-      ),
+    
+    // Pass location and child to PrimaryScaffold
+    return PrimaryScaffold(
+      body: child,
+      currentLocation: location,
     );
   }
 }
